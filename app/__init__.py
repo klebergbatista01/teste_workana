74eb840712322e4dc57304a84bc457fd6c28229f
klebergbatista01 <101155208+klebergbatista01@users.noreply.github.com>
2022-04-12 16:50:22 -0300

    Atualização

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from app.drug_analyzer import DrugAnalyzer
+from test import test_drug_analyzer, test_structure, test_verify_series
+
+
+analyze1= input(test_verify_series)
+analize2= input(test_drug_analyzer)
+analize3= input(test_structure)
+
+
+print(analyze1, analize2, analize3)
--- app/drug_analyzer.py
+++ app/drug_analyzer.py
@@ -1,6 +1,6 @@
 class DrugAnalyzer:
     # TODO: Part 1 - Add method(s) necessary to fulfill the requirements.
-
+     
     def verify_series(
         self,
         series_id: str,
@@ -8,5 +8,6 @@ class DrugAnalyzer:
         act_subst_rate: float,
         allowed_imp: float,
     ) -> bool:
-        # TODO: Part 2 - Implement this method.
+        # TODO: #1 Part 2 - Implement this method.
+        
         return True
